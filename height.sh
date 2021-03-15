ea9b72f8184c12d93bd55aa55f46e5dc5393a212
julyawang <1090667500@qq.com>
2021-03-15 19:48:29 +0800

    zhishu

--- height.sh
+++ height.sh
@@ -26,6 +26,31 @@ done
 
 
 
+// new
+i=1
+num=$[RANDOM%100+1]
+var_array=($num)
+for file in $(ls /home/zokrates/.zokrates/examples/ifh*.zok |sort -n -t'h' -k 3);do
+    for j in 0..$i;do
+    num=$[RANDOM%100+1]
+    var_array+=($num)
+    done
+    echo "file: $file" >> height_result.txt
+    echo "compile" >> height_result.txt
+zokrates compile -i $file | tail -n1  >> height_result.txt
+    (time zokrates compile -i $file)  >> height_result.txt
+echo "setup" >> height_result.txt
+zokrates setup | tail -n2  >> height_result.txt
+    (time zokrates setup)  >> height_result.txt
+    echo "compute-witness" >> height_result.txt
+zokrates compute-witness -a ${var_array[@]} | tail -n4  >> height_result.txt
+    (time zokrates compute-witness -a ${var_array})  >> height_result.txt
+    echo "generate-proof" >> height_result.txt
+  zokrates generate-proof | tail -n1  >> height_result.txt
+(time zokrates generate-proof) >> height_result.txt
+ i+=i
+done
+
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 use std::fs::File;
-
+use num_traits::pow;
 
 use std::io::Write;
 extern crate rand;
@@ -8,43 +8,39 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
-//    width_main(0,100);
-//    height_main(0, 100);
-    width_main(100,200);
-//    height_main(100,200);
-//    width_main(200, 300);
-//    height_main(200, 300);
-//    width_main(300, 400);
-//    height_main(300, 400);
-//    width_main(400, 500);
-//    height_main(400, 500);
-//    width_main(500, 600);
-//    height_main(500, 600);
-//    width_main(600, 700);
-//    height_main(600, 700);
-//    width_main(700, 800);
-//    height_main(700, 800);
-//    width_main( 800, 900);
-//    height_main(800, 900);
-//    width_main(900, 1000);
-//    height_main(900, 1000);
+//    height_main(6);
+    height_main(7);
+//    height_main(8);
+//    height_main(9);
+//    height_main(10);
+//    height_main(11);
+//    height_main(12);
+//    height_main(13);
+//    height_main(14);
+//    height_main(15);
+//    height_main(16);
+
 }
 
-fn width_main(min: u64, max: u64) {
+fn width_main(max: usize) {
     let mut file_name: String = "ifw".to_string();
     let mut var: String = "a".to_string();
+    let mut rng = rand::thread_rng();
+    let min = pow(2u64, max - 1) as u64;
+    let max = pow(2u64, max.clone()) as u64;
+    let mut random = rng.gen_range::<u64>(min, max);
     for i in 0..101 {
         file_name = format!("ifw{}.zok", i);
 //        println!("{}", file_name);
         let mut _file = File::create(file_name.clone()).expect("create failed");
         let mut content = "def main(private field a0".to_string();
-        let mut body = "    field res = (if a0 >= 20 then 1 else 0 fi)".to_string();
+        let mut body = format!("    field res = (if a0 >= {} then 1 else 0 fi)", random);
         for j in 1..i {
             var = format!("a{}",j);
 //            println!("{}", var);
             content = format!("{}, {}", content, format!("private field {}", var));
             let mut rng = rand::thread_rng();
-            let random = rng.gen_range(min, max);
+            let mut random = rng.gen_range::<u64>(min, max);
             body = format!("{} + (if {} >= {} then 1 else 0 fi)", body, var, random);
         }
         content = format!("{}) -> (field): ", content);
@@ -53,34 +49,36 @@ fn width_main(min: u64, max: u64) {
     }
 
 }
-
-fn height_main(min:u64, max: u64) {
+fn height_main(max: usize) {
     let mut file_name: String = "ifh".to_string();
     let mut var: String = "a".to_string();
     let mut rng = rand::thread_rng();
-    for i in 0..101 {
+    let min = pow(2u64, max - 1) as u64;
+    let max = pow(2u64, max.clone()) as u64;
+    let mut random = rng.gen_range::<u64>(min, max);
+    for i in [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100].iter() {
         file_name = format!("ifh{}.zok", i);
         let mut _file = File::create(file_name.clone()).expect("create failed");
         let mut content = "def main(private field a0".to_string();
-        let mut body = "    field res = (if a0 >= 18 then ".to_string();
-        for j in 1..i {
-            var = format!("a{}",j);
+        let mut body = format!("    field res = (if a0 >= {} then ", random);
+        for j in 1..*i {
+            var = format!("a{}", j);
             content = format!("{}, {}", content, format!("private field {}", var));
             let random = rng.gen_range(min, max);
             body = format!("{}(if {} < {} then ", body, var, random);
-            if j == i-1 {
+            if j == i - 1 {
                 body = format!("{} 1 else 0 fi)", body);
                 for k in 0..j {
                     body = format!("{} else 0 fi)", body);
                 }
             }
         }
-        body = format!("{}\n    field verify = if res == a{} then 1 else 0 fi", body, i+1);
+        body = format!("{}\n    field verify = if res == a{} then 1 else 0 fi", body, i + 1);
         body = format!("{}\n    return verify", body);
-        content = format!("{}, field a{}) -> (field): ", content, i+1);
+        content = format!("{}, field a{}) -> (field): ", content, i + 1);
         let full = format!("{}\n{}\n", content, body);
         _file.write_all(full.as_bytes()).expect("write failed");
     }
-
 }
+
 
